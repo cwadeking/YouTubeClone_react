d8d85ff6e87c06fbe9870f80af9eefa76cfec2d6
cwadeking <charles.w.king.cwk@gmail.com>
2020-10-08 18:13:49 -0500

    pulled to fix tag issues in the jsx

--- src/App.js
+++ src/App.js
@@ -6,51 +6,47 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: [],
-    };
+      data: []
+    }
   }
   componentDidMount() {
-    axios.get(`https://www.googleapis.com/youtube/v3/search?q={search query}&key={AIzaSyClhAQZ87dYqVrOTUa5kHGjRUDwPV8wmE8}`).then((res) => {
+    axios.get(`https://www.googleapis.com/youtube/v3/search?q={search query}&key={AIzaSyClhAQZ87dYqVrOTUa5kHGjRUDwPV8wmE8}`)
+    .then((res) => {
       const responseData = res.data;
       this.setState({ 
         data: responseData
        });
-    });
+    })
   }
-
-
-
-  
 
   render() {
     return (
-      <div className="header">
-        <h1 style={{ backgroundColor: "forestgreen" }}>Clonetube!</h1>
+    
+      <div id = "main-content">
 
-
+        <div className="header">
+          <h1>Clonetube!</h1>
+        </div>
 
         <div class="split left">
           <div class="centered">
-            <img src="img_avatar2.png" alt="Avatar woman">
-           <h2>Jane Flex</h2>
-           <p>Some text.</p>
+            <img src="img_avatar2.png" alt="Avatar woman"></img>
+            <h2>Jane Flex</h2>
+            <p>Some text.</p>
+          </div>
+        </div>
+
+        <div class="split right">
+          <div class="centered">
+            <img src="img_avatar.png" alt="Avatar man"></img>
+            <h2>John Doe</h2>
+            <p>Some text here too.</p>
+          </div>
+        </div>
+        
       </div>
-</div>
-
-<div class="split right">
-  <div class="centered">
-    <img src="img_avatar.png" alt="Avatar man">
-    <h2>John Doe</h2>
-    <p>Some text here too.</p>
-  </div>
-</div>
-
-
-
-
-
-      </div>
-    );
+   
+    )
   }
 }
 export default App;
