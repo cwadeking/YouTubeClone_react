9292ddd68abb51e13ac4c571f4e6cf0acc5a2ac4
Daniel Christine <danieljameschristine@gmail.com>
2020-10-08 17:57:28 -0500

    Working on search function.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+    };
+  }
+  componentDidMount() {
+    axios.get(`https://www.googleapis.com/youtube/v3/search?q={search query}&key={AIzaSyClhAQZ87dYqVrOTUa5kHGjRUDwPV8wmE8}`).then((res) => {
+      const responseData = res.data;
+      this.setState({ 
+        data: responseData
+       });
+    });
+  }
+
+
+
+  
+
+  render() {
+    return (
+      <div className="header">
+        <h1 style={{ backgroundColor: "forestgreen" }}>Clonetube!</h1>
+
+
+
+        <div class="split left">
+          <div class="centered">
+            <img src="img_avatar2.png" alt="Avatar woman">
+           <h2>Jane Flex</h2>
+           <p>Some text.</p>
+      </div>
+</div>
+
+<div class="split right">
+  <div class="centered">
+    <img src="img_avatar.png" alt="Avatar man">
+    <h2>John Doe</h2>
+    <p>Some text here too.</p>
+  </div>
+</div>
+
+
+
+
+
+      </div>
+    );
+  }
 }
-
 export default App;
